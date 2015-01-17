46b2ab459fd18c8bf82d05e5117a22670ad64dbc
pekepeke <pekepekesamurai+github@gmail.com>
2015-01-17 19:08:54 +0900

    add rename script

---
+++ rename.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+opt_restore=0
+usage() {
+  prg_name=`basename $0`
+  cat <<EOM
+  Usage: $prg_name [-h]
+-r : rename genv* to goenv*
+EOM
+  exit 1
+}
+
+main() {
+  local from=goenv
+  local to=genv
+  local script=$(basename $0)
+  if [ $opt_restore -eq 1 ]; then
+    from=genv
+    to=goenv
+  fi
+  for f in $(find . -name ".git" -prune -prune -o -type f); do
+    if [ $(basename $f) == $script -o -d $f ]; then
+      continue
+    fi
+    sed -i "s/$from/$to/g" $f
+    mv $f $(echo $f | sed "s/$from/$to/")
+  done
+  cd bin
+  rm $from
+  ln -s ./../libexec/$to $to
+}
+
+OPTIND_OLD=$OPTIND
+OPTIND=1
+while getopts "hr" opt; do
+  case $opt in
+    h) usage ;;
+    r) opt_restore=1;;
+  esac
+done
+shift `expr $OPTIND - 1`
+OPTIND=$OPTIND_OLD
+if [ $OPT_ERROR ]; then
+  usage
+fi
+
+main "$@"
+
